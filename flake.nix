40cd304264851b4eaa16241bc5c94afa27e86626
raf <rraf@tuta.io>
2025-02-04 10:04:54 -0800

    chore: added flake

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "Build Shell with any dependency of the project";
+
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs =
+            with pkgs;
+            [
+              fuse
+			  pkg-config
+            ];
+        };
+      }
+    );
+}
